8f34442997a88b6971650cf37802ea19686b3972
h5lianfu <h5lianfu@gmail.com>
2024-11-30 20:35:09 -0500

    update home page

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -14,7 +14,13 @@ const DPaper = styled(Paper)({
   display: 'flex',
   position: 'relative',
   margin: `${Size.topHeight} ${Size.rightWidth} ${Size.statusHeight} ${Size.menuWidth}`,
+  padding: '1rem',
   overflowY: 'auto',
+});
+
+const DStack = styled(Stack)({
+  display: 'flex',
+  alignItems: 'flex-start',
 });
 
 const Home = (props: IProps, state: IState) => {
@@ -24,93 +30,101 @@ const Home = (props: IProps, state: IState) => {
 
   return (
     <DPaper>
-      <Typography variant="h6">可琪, I miss you...</Typography>
-      <Box>
-        <Button variant="contained" color="primary">可琪, I miss you</Button>
-        <Button variant="contained" color="secondary">可琪, I miss you</Button>
-        <Button variant="contained" color="success">可琪, I miss you</Button>
-        <Button variant="contained" color="info">可琪, I miss you</Button>
-        <Button variant="contained" color="warning">可琪, I miss you</Button>
-        <Button variant="contained" color="error">可琪, I miss you</Button>
-      </Box>
-      <Box>
-        <Button variant="contained" color="black">可琪, I miss you</Button>
-        <Button variant="contained" color="dark">可琪, I miss you</Button>
-        <Button variant="contained" color="gray">可琪, I miss you</Button>
-        <Button variant="contained" color="white">可琪, I miss you</Button>
-        <Button variant="contained" color="oliver">可琪, I miss you</Button>
-        <Button variant="contained" color="green">可琪, I miss you</Button>
-      </Box>
-      <Box>
-        <Button color="primary">可琪, I miss you...</Button>
-        <Button color="secondary">可琪, I miss you...</Button>
-        <Button color="success">可琪, I miss you...</Button>
-        <Button color="info">可琪, I miss you...</Button>
-        <Button color="warning">可琪, I miss you...</Button>
-        <Button color="error">可琪, I miss you...</Button>
-      </Box>
-      <Box>
-        <Button variant="outlined" color="primary">可琪, I miss you</Button>
-        <Button variant="outlined" color="secondary">可琪, I miss you</Button>
-        <Button variant="outlined" color="success">可琪, I miss you</Button>
-        <Button variant="outlined" color="info">可琪, I miss you</Button>
-        <Button variant="outlined" color="warning">可琪, I miss you</Button>
-        <Button variant="outlined" color="error">可琪, I miss you</Button>
-      </Box>
-      <Box>
-        <Button variant="outlined" color="black">可琪, I miss you...</Button>
-        <Button variant="outlined" color="dark">可琪, I miss you...</Button>
-        <Button variant="outlined" color="gray">可琪, I miss you...</Button>
-      </Box>
-      <Box>
-        <Button variant="dashed" color="primary">可琪, I miss you</Button>
-        <Button variant="dashed" color="secondary">可琪, I miss you</Button>
-        <Button variant="dashed" color="success">可琪, I miss you</Button>
-        <Button variant="dashed" color="info">可琪, I miss you</Button>
-        <Button variant="dashed" color="warning">可琪, I miss you</Button>
-        <Button variant="dashed" color="error">可琪, I miss you</Button>
-      </Box>
-      <Box>
-        <Button variant="dashed" color="black">可琪, I miss you...</Button>
-        <Button variant="dashed" color="dark">可琪, I miss you...</Button>
-        <Button variant="dashed" color="gray">可琪, I miss you...</Button>
-      </Box>
-      <Stack>
-        <Typography variant="h1">可琪, I miss you...</Typography>
-        <Typography variant="h2">可琪, I miss you...</Typography>
-        <Typography variant="h3">可琪, I miss you...</Typography>
-        <Typography variant="h4">可琪, I miss you...</Typography>
-        <Typography variant="h5">可琪, I miss you...</Typography>
-        <Typography variant="h6">可琪, I miss you...</Typography>
-        <Typography variant="body1">可琪, I miss you...</Typography>
-        <Typography variant="body2">可琪, I miss you...</Typography>
-        <Typography variant="caption">可琪, I miss you...</Typography>
-        <Typography variant="overline">可琪, I miss you...</Typography>
-        <Typography variant="subtitle1">可琪, I miss you...</Typography>
-        <Typography variant="subtitle2">可琪, I miss you...</Typography>
-      </Stack>
-      <Stack>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 我不想再前往宫殿了，我只愿停留在有你的客栈 ~~~</Typography>
-        <Typography variant="h5">~~~ 诗为她而作，泪为你而流 ~~~</Typography>
-        <Typography variant="h5">~~~ 诗为她而作，泪为你而流 ~~~</Typography>
-        <Typography variant="h5">~~~ 诗为她而作，泪为你而流 ~~~</Typography>
-        <Typography variant="h5">~~~ 诗为她而作，泪为你而流 ~~~</Typography>
-        <Typography variant="h5">~~~ 诗为她而作，泪为你而流 ~~~</Typography>
-        <Typography variant="h5">~~~ 诗为她而作，泪为你而流 ~~~</Typography>
-        <Typography variant="h6">一滴泪，滴入河流，汇入大海，凝入云朵，化为雨滴，落在你的脸颊</Typography>
-        <Typography variant="h6">一滴泪，滴入河流，汇入大海，凝入云朵，化为雨滴，落在你的脸颊</Typography>
-        <Typography variant="h6">一滴泪，滴入河流，汇入大海，凝入云朵，化为雨滴，落在你的脸颊</Typography>
-        <Typography variant="h6">一滴泪，滴入河流，汇入大海，凝入云朵，化为雨滴，落在你的脸颊</Typography>
-        <Typography variant="h6">一滴泪，滴入河流，汇入大海，凝入云朵，化为雨滴，落在你的脸颊</Typography>
-        <Typography variant="h6">一滴泪，滴入河流，汇入大海，凝入云朵，化为雨滴，落在你的脸颊</Typography>
-      </Stack>
-      <Button>End End</Button>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="contained" color="primary">独自走在一起走过的长廊</Button>
+        <Button variant="contained" color="secondary">仿佛你就在我的身边</Button>
+        <Button variant="contained" color="success">转身想要牵你的手</Button>
+        <Button variant="contained" color="info">才发现</Button>
+        <Button variant="contained" color="warning">那只是曾经</Button>
+        <Button variant="contained" color="error">那只是曾经的曾经...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="contained" color="black">记忆的碎片经常侵入我的生活</Button>
+        <Button variant="contained" color="dark">闯入我的梦中</Button>
+        <Button variant="contained" color="gray">带走我的泪水...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="contained" color="primary">心, 是一个人强大的基础</Button>
+        <Button variant="contained" color="warning">思想, 是人们超越平凡的阶梯</Button>
+        <Button variant="contained" color="error">灵魂, 是人类探索宇宙的载体...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button color="primary">你来过</Button>
+        <Button color="secondary">一缕烟</Button>
+        <Button color="success">过心间</Button>
+        <Button color="info">具思念</Button>
+        <Button color="warning">忆千年...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="outlined" color="primary">思, 非一时之所梦</Button>
+        <Button variant="outlined" color="secondary">梦, 非一时之所思</Button>
+        <Button variant="outlined" color="success">长梦而烧脑, 至思而灼心</Button>
+        <Button variant="outlined" color="info">虽长梦而不语</Button>
+        <Button variant="outlined" color="warning">虽至思而不见...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="outlined" color="error">当暴雨不再影响我的心情</Button>
+        <Button variant="outlined" color="black">当离别不再令我感到伤感</Button>
+        <Button variant="outlined" color="dark">当金钱不足以撼动我的梦想</Button>
+        <Button variant="outlined" color="gray">这才是我喜欢的自己...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="dashed" color="primary">挫其志</Button>
+        <Button variant="dashed" color="secondary">锻其心</Button>
+        <Button variant="dashed" color="success">阻其行</Button>
+        <Button variant="dashed" color="info">厉其言</Button>
+        <Button variant="dashed" color="warning">而屹立不倒...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="dashed" color="error">很喜欢很喜欢一个人, 却不能在一起, 是怎样的心情?</Button>
+        <Button variant="dashed" color="black">没有后悔, 没有遗憾, 只有对她止不住的思念...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="dashed" color="primary">一人饮酒醉</Button>
+        <Button variant="dashed" color="secondary">十年相思难追忆</Button>
+        <Button variant="dashed" color="info">昨日梦中与会</Button>
+        <Button variant="dashed" color="warning">仿佛今生, 恍若隔世...</Button>
+      </DStack>
+      <DStack>
+        <Button variant="contained" color="primary">我不想再前往宫殿了</Button>
+        <Button variant="contained" color="secondary">我只愿停留在有你的客栈...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="contained" color="info">行行走走忙忙匆匆挤挤</Button>
+        <Button variant="contained" color="warning">进进出出影影形形觅觅...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="contained" color="error">你是夏天的棉袄</Button>
+        <Button variant="outlined" color="primary">沸血, 煮心, 烧脑...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="outlined" color="secondary">诗为她而作, 泪为你而流...</Button>
+        <Button variant="outlined" color="warning">拿起若有意, 放下又何须用心...</Button>
+        <Button variant="outlined" color="error">香烟遇到了火柴, 点亮了自己...</Button>
+      </DStack>
+      <DStack>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="outlined" color="warning">心中有你, 如影随形</Button>
+        <Button variant="outlined" color="error">梦中有你, 朝夕相伴</Button>
+        <Button variant="outlined" color="black">未来有你, 事倍功半</Button>
+        <Button variant="outlined" color="dark">时间如你, 不能相见...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="dashed" color="warning">一滴眼泪的溢出, 蕴含的是百分的思念</Button>
+        <Button variant="dashed" color="error">两个眼角的悲伤, 望穿的是一世的秋凉...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button variant="contained" color="primary">一滴泪</Button>
+        <Button variant="contained" color="secondary">滴入河流</Button>
+        <Button variant="contained" color="info">汇入大海</Button>
+        <Button variant="contained" color="warning">凝入云朵</Button>
+        <Button variant="contained" color="error">化为雨滴</Button>
+        <Button variant="contained" color="black">落在你的脸颊...</Button>
+        <Typography variant="body1">------ 可琪, I miss you ------</Typography>
+        <Button color="primary">谁愿意抬一脚向前</Button>
+        <Button color="secondary">谁愿意转一眼相见</Button>
+        <Button color="error">谁愿意动一指传言...</Button>
+      </DStack>
     </DPaper>
   );
 };
